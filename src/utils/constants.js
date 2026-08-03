d9dc2ccd9199e31aa02857a6884e70cbd6b43ddc
MonuKD-2026 <monukd01dev@gmail.com>
2026-08-04 00:10:52 +0530

    BASE_URL made dynamic for the nginx and development

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
-export const BASE_URL = 'http://localhost:8080/api/v1';
+export const BASE_URL = import.meta.env.MODE === 'development' 
+  ? 'http://localhost:8080/api/v1' 
+  : '/api/v1';
 export const DELETE_CONFIRM_TEXT = "delete my account";
 export const NETWORK_ERR_MESG = "Network Error! DevTinder server is unreachable."
